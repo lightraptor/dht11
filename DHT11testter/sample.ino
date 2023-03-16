678a28cccda9cf0c570431c787c90553eb799e46
Lê Nguyễn Quang Duy <75661466+lightraptor2310@users.noreply.github.com>
2023-03-16 16:17:22 +0700

    upload code example

---
+++ DHT11testter/sample.ino
@@ -0,0 +1,38 @@
+// code mau do nhiet do cho dht11 
+
+#include "DHT11.h"
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT11 dht(DHTPIN);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHT11 test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
